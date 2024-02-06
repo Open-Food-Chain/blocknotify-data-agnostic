e03344e2935b871025eb9a733944333b0c51b4a7
diesmaster <chris@vanmaarseveen.net>
2024-02-06 09:33:28 +0100

    setup for orderbook

--- address_book.py
+++ address_book.py
@@ -3,19 +3,56 @@ class AddressBook:
         self.wallet = wallet
         self.addresses = {}
 
-    def add_address(self, name, address):
-        """ Adds an address with a corresponding name to the address book. """
-        self.addresses[name] = address
+    def check_balance(self):
+        balance = self.wallet.get_balance()
+        if balance == 0:
+            return None
+        else:
+            return f"Balance is enough: {balance}"
 
-    def get_address(self, name):
-        """ Retrieves an address by its name from the address book. """
-        return self.addresses.get(name, None)
+    def create_oracle(self, name, description, data_type):
+        return self.wallet.oracle_create(name, description, data_type)
 
-    def remove_address(self, name):
-        """ Removes an address from the address book. """
-        if name in self.addresses:
-            del self.addresses[name]
+    def send_oracle_creation_tx(self, hex_value):
+        return self.wallet.sendrawtxwrapper(hex_value)
 
-    def list_addresses(self):
-        """ Lists all addresses in the address book. """
-        return self.addresses
+    def fund_oracle(self, oracle_txid):
+        return self.wallet.oracle_fund(oracle_txid)
+
+    def register_as_publisher(self, oracle_txid, data_fee):
+        return self.wallet.oracle_register(oracle_txid, data_fee)
+
+    def send_register_tx(self, register_hex):
+        return self.wallet.sendrawtxwrapper(register_hex)
+
+    def subscribe_to_oracle(self, oracle_txid, publisher_id, fee):
+        return self.wallet.oracle_subscribe(oracle_txid, publisher_id, fee)
+
+    def publish_data_to_oracle(self, oracle_txid, data_string):
+        # Convert the string to UTF-8 bytes and then to a hex string
+        hex_data = data_string.encode('utf-8').hex()
+
+        # Get the length of the original data string
+        data_length = len(data_string)
+
+        # Convert the length to a hex string
+        length_hex = format(data_length, 'x')
+
+        # Concatenate the length in hex and the data hex string
+        final_hex_data = length_hex + hex_data
+
+        # Send the data to the oracle
+        return self.wallet.oracle_data(oracle_txid, final_hex_data)
+
+    def get_oracle_info(self, oracle_txid):
+        return self.wallet.oracle_info(oracle_txid)
+
+    def create_string_oracle(self, name, description):
+        # Call the oracle_create method from the wallet object
+        res = self.wallet.oracle_create(name, description, "s")
+
+        # Check if the creation was successful
+        if not res.get('result') == 'success':
+            error_message = res.get('error', 'Unknown error')
+            print(f"Oracle creation failed: {error_message}")
+            raise Exception(f"Oracle creation failed: {error_message}")
--- rpc_test.py
+++ rpc_test.py
@@ -5,7 +5,12 @@ import platform
 from slickrpc import Proxy
 
 from komodo_py.node_rpc import NodeRpc
+from komodo_py.explorer import QueryInterface
+from komodo_py.explorer import Explorer
 from komodo_py.wallet import WalletInterface
+from komodo_py.oracles import Oracles
+
+from address_book import AddressBook
 
 import os
 from dotenv import load_dotenv
@@ -21,19 +26,42 @@ ip_address = os.getenv('BATCH_SMARTCHAIN_NODE_IPV4_ADDR')
 
 node_rpc = NodeRpc(user_name, password, rpc_port, private_key, "127.0.0.1")
 
-wal_in = WalletInterface(node_rpc, "pact_image_wheat_cheese_model_daring_day_only_setup_cram_leave_good_limb_dawn_diagram_kind_orchard_pelican_chronic_repair_rack_oxygen_intact_vanish")
+#query = QueryInterface(node_rpc)
 
-res = wal_in.get_address()
+#oracle_man = Oracles(query)
 
-print(res)
+ex = Explorer("https://ofcmvp.explorer.batch.events/")
 
-res = wal_in.get_balance()
 
-print(res)
 
-# Make the getinfo call
-#try:
-#    info = node_rpc.getinfo()
-#    print("Node Info:", info)
-#except Exception as e:
-#    print(f"Error: {e}")
+ret = node_rpc.getinfo()
+print(ret)
+
+#ret = node_rpc.get_rawtransaction("fb5e0c9fbcf43ade68e7ab9b5bd5774264f80568743c3e8f1b1ef377b604d288")
+#print(ret)
+
+wal_in = WalletInterface(node_rpc, "pact_image_wheat_cheese_model_daring_day_only_setup_cram_leave_good_limb_dawn_diagram_kind_orchard_pelican_chronic_repair_rack_oxygen_intact_vanish", True)
+
+#ret = wal_in.create_string_oracle("filler", "this is a filler oracle", "0.9")
+
+#print("txid: " + str(ret))
+
+#ret = wal_in.get_oracle_info("695a79f039dbc241465f844c111cbfab0273942b67ca05f4c7b5b4b86b1b2e4b")
+#print(ret)
+
+ret = wal_in.subscribe_to_oracle("695a79f039dbc241465f844c111cbfab0273942b67ca05f4c7b5b4b86b1b2e4b", "1")
+print(ret)
+
+ret = node_rpc.get_rawtransaction(ret)
+print(ret)
+
+ret = ex.broadcast(ret)
+print(ret)
+
+#ret = wal_in.publish_data_string_to_oracle("695a79f039dbc241465f844c111cbfab0273942b67ca05f4c7b5b4b86b1b2e4b", "test")
+#print(ret)
+
+#wal_in = WalletInterface(node_rpc, "pact_image_wheat_cheese_model_daring_day_only_setup_cram_leave_good_limb_dawn_diagram_kind_orchard_pelican_chronic_repair_rack_oxygen_intact_vanish")
+
+#addy_book = AddressBook(wal_in)
+
